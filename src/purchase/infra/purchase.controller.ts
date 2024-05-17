20f27bc300d72b03aa759900908b3150922136d2
wesley <wesley.silva@cartaovaibem.com.br>
2024-05-16 21:37:55 -0300

    feat: init purchase module

--- src/purchase/infra/purchase.controller.ts
+++ src/purchase/infra/purchase.controller.ts
@@ -1,16 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreatePurchaseService } from '../domain/services/create-purchase.service';
 import { ApiTags } from '@nestjs/swagger';
+import { ListPurchaseService } from '../domain/services/list-purchase.service';
 
 @Controller('purchase')
 @ApiTags('purchase')
 export class PurchaseController {
     constructor(
         private readonly createPurchaseService: CreatePurchaseService,
+        private readonly listPurchaseService: ListPurchaseService,
     ) {}
 
     @Post('create')
     async create(@Body() body: any) {
         return this.createPurchaseService.execute(body);
+    }
+
+    @Get()
+    async list() {
+        return this.listPurchaseService.execute();
     }
 }
