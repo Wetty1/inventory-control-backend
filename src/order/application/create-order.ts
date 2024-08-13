2dd9456ea0941ebb9d252098711842c726bba5ea
wesley <contato.wesleydesousa@gmail.com>
2024-08-13 20:29:27 -0300

    chore: create entity

---
+++ src/order/application/create-order.ts
@@ -0,0 +1,34 @@
+import { Order } from '../domain/order';
+import { OrderRepository } from '../domain/order-repoository';
+
+type Input = {
+    total: number;
+    numberPhone: string;
+    address: string;
+    date: Date;
+    items: string[];
+};
+
+type Output = {
+    id: string;
+};
+
+export class CreateProduct {
+    constructor(private readonly orderRepository: OrderRepository) {}
+
+    async execute(input: Input): Promise<Output> {
+        const output = await this.orderRepository.saveOrder(
+            Order.create(
+                input.total,
+                input.numberPhone,
+                input.address,
+                input.date,
+                input.items,
+            ),
+        );
+
+        return {
+            id: output.id,
+        };
+    }
+}
