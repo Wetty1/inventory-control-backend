be2bc19056e4db74474d125371ebdb5dceb807a1
wesley <contato.wesleydesousa@gmail.com>
2024-09-28 23:57:24 -0300

    chore: adds list supplies use case

--- src/stock1/domain/supply.repository.ts
+++ src/stock1/domain/supply.repository.ts
@@ -1,6 +1,7 @@
 import { Supply } from './supply';
 
 export interface SupplyRepository {
+    list(): Promise<Supply[]>;
     get(id: any): Promise<Supply>;
     getByCategory(categoryId: any): Promise<Supply[]>;
     getByName(name: string): Promise<Supply>;
